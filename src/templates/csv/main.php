92265b294ad71fa68eb572fb8b2b1ab557916b65
vitor.farias <vitor.farias@butantan.gov.br>
2024-03-14 11:23:22 -0300

    [FIX] Correção dos relatórios nao estarem puxando departamento

--- src/templates/csv/main.php
+++ src/templates/csv/main.php
@@ -5,7 +5,7 @@ mb_convert_encoding($file, 'UTF-16LE', 'UTF-8');
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header('Content-Disposition: attachment; filename=' . $file);
 
-$content = "Evento ID; Nome do Evento; Local; Categoria; Qtd. Participantes; Status; Solicitante; Email Solicitante; Ramal Solicitante; Responsável; Email Responsável; Ramal Responsável; Data Incial; Data Final; Hora Inicial; Hora Final; Participante PcD; Detalhes PcD; Café; Observação; \n";
+$content = "Evento ID; Nome do Evento; Local; Categoria; Qtd. Participantes; Status; Solicitante; Departamento; Email Solicitante; Ramal Solicitante; Responsável; Email Responsável; Ramal Responsável; Data Incial; Data Final; Hora Inicial; Hora Final; Participante PcD; Detalhes PcD; Café; Observação; \n";
 
 
 foreach ($results as $result) {
@@ -40,6 +40,7 @@ foreach ($results as $result) {
     $result['quantidade'] . ";" .
     $status . ";" .
     $result['solicitante'] . ";" .
+    $result['departamento'] . ";" .
     $result['email'] . ";" .
     $result['ramal'] . ";" .
     $result['responsavel'] . ";" .
