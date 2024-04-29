f0c15f2542f85f0efbed8316d1f74d8032783eb9
vitor.farias <vitor.farias@butantan.gov.br>
2024-04-29 15:23:49 -0300

    [FIX] Correção da visualização dos campos do selectize ocultos em baixo do footer

--- src/templates/default.php
+++ src/templates/default.php
@@ -54,7 +54,7 @@ $page = basename(parse_url($url, PHP_URL_PATH));
 
 </body>
 
-<footer style="background-color: white; z-index: 101; position: relative; display: flex; align-items: center; justify-content: center; flex-direction: column; box-shadow: -1px -1px 1px rgba(0,0,0,0.3)" class="py-4">
+<footer style="background-color: white; z-index: 1; position: relative; display: flex; align-items: center; justify-content: center; flex-direction: column; box-shadow: -1px -1px 1px rgba(0,0,0,0.3)" class="py-4">
   <div style="display: flex; align-items: center; justify-content: center;">
     <img src="/assets/img/logo-footer-biblioteca.png" alt="Logo Biblioteca" width="65px" style="margin-right: 20px;">
     <img src="/assets/img/logo-ib.png" alt="Logo Instituto Butantan" width="110px" style="margin-right: 20px;">
