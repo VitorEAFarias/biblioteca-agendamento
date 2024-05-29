876b5371bd42a8cda7955884bdb872eaafb6532c
vitor.farias <vitor.farias@butantan.gov.br>
2024-05-29 08:58:30 -0300

    [UPDATE] Alteração de logos

--- src/templates/default.php
+++ src/templates/default.php
@@ -54,13 +54,15 @@ $page = basename(parse_url($url, PHP_URL_PATH));
 
 </body>
 
-<footer style="background-color: white; z-index: 1; position: relative; display: flex; align-items: center; justify-content: center; flex-direction: column; box-shadow: -1px -1px 1px rgba(0,0,0,0.3)" class="py-4">
+<footer style="background-color: white; z-index: 1; position: relative; display: flex; align-items: center; justify-content: center; flex-direction: column; box-shadow: -1px -1px 1px rgba(0,0,0,0.3)" class="py-3">
   <div style="display: flex; align-items: center; justify-content: center;">
     <img src="/assets/img/logo-footer-biblioteca.png" alt="Logo Biblioteca" width="65px" style="margin-right: 20px;">
-    <img src="/assets/img/logo-ib.png" alt="Logo Instituto Butantan" width="110px" style="margin-right: 20px;">
-    <img src="/assets/img/logo_fb.png" alt="Logo Fundação Butantan" width="150px">
+    <div class="mb-4">
+      <img src="https://fundacaobutantan.org.br/assets/img/logos/ib-fundacao-black.png" alt="Logo Instituto Butantan" width="300px" style="margin-right: 20px;">
+
+    </div>
   </div>
-  <div style="margin-top: 10px;">
+  <div style="margin-top: 5px;">
     <span>Criado por Tic Desenvolvimento. Todos os direitos reservados ao © Instituto Butantan 2023</span>
   </div>
 </footer>
